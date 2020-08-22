fb80735c0537dbae9ae9599174cb5e5f1fe0e242
kino-ma <kino.ma.ms@gmail.com>
2020-08-22 20:54:28 +0900

    exampleのコードを移植

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+mod threshold_enc;
+mod threshold_sig;
+
 fn main() {
-    println!("Hello, world!");
+    crate::threshold_enc::threshold_encode();
+    crate::threshold_sig::threshold_signature();
 }
